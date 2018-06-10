76bfe9c6b3f9e709363ac4ded417252715dc57d7
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-10 13:50:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
 def reverse_each_word(phrase)
   newphrase=""
   arph=phrase.split(" ")
+<<<<<<< HEAD
   arph.collect do |word|
      word.reverse!
    end
   arph.join(" ")
 
+=======
+  arphn=[]
+  arph.collect.join(" ") do |word|
+    newphrase << word.reverse!
+      end
+  return newphrase
+>>>>>>> b6651e722e218dade03547fb1fa507401c1fa021
 end
